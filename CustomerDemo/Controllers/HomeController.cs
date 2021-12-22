35c3b63957e88a24dbbe52731dad34b6622b1b7d
poonam.d <snehalshendge18@gmail.com>
2021-12-22 17:48:35 +0530

    AddDeleteMethod

--- CustomerDemo/Controllers/HomeController.cs
+++ CustomerDemo/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace CustomerDemo.Controllers
         {
             return View();
         }
+        public IActionResult PrivacyDelete()
+        {
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
